288d3832c338cac0dafb2e9000125b78d23c12e2
PatrickTUy <yangeney@gmail.com>
2022-02-03 12:18:19 +0200

    worked on auth,validation and tests

--- src/services/commentServices.js
+++ src/services/commentServices.js
@@ -1,17 +1,24 @@
-import Comment from "../models/comment.js";
+import Article from "../models/article.js"
+import Comment from "../models/comment.js"
 
-export const createCommentService = async (data) => {
-  const comment = await Comment(data);
-  comment.save();
-  return comment;
-};
+export const createCommentService = async (id,newComment) => {
+    const comment = await Comment(newComment)
+    const articleToUpdate = await Article.findById(id);
+    console.log(comment)
+    articleToUpdate.comments.push(comment)
+    await articleToUpdate.save()
 
-export const getOneCommentService = async (id) => {
-  const comment = await Comment.findOne({ _id: id });
-  return comment;
-};
+    await comment.save()
 
-export const getAllCommentsService = async () => {
-  const comment = await Comment.find();
-  return comment;
-};
+    return comment
+         
+}
+
+export const getAllArticleCommentsService = async (id)=>{
+
+    const article= await Article.findById(id)
+    const IDs=article.comments
+    const comments=Comment.find({'_id':{$in: IDs }})
+    
+    return comments
+}
