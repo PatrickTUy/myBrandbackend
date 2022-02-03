f114c0c4ae4ec72342796b5275557e1c8e807ef3
PatrickTUy <yangeney@gmail.com>
2022-02-03 12:18:19 +0200

    worked on auth,validation and tests

--- src/models/query.js
+++ src/models/query.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const schema = mongoose.Schema({
-  title: String,
-  content: String,
-  create_at: Date,
+	senderName: String,
+	email: String,
+	message: String,
+	location: String,
+	date_created: { type: Date, default: Date.now() }
 });
 
 export default mongoose.model("Query", schema);
