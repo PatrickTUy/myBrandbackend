b878dea0017f3af349b8c21d32bbe4e00ed97a11
PatrickTUy <yangeney@gmail.com>
2022-02-05 14:24:32 +0200

    added cli

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,31 +1,31 @@
-// import chai, { expect } from "chai";
-// import chaiHttp from "chai-http";
-// import app from "../src/app.js";
-// import "dotenv/config";
-// use(chaiHttp);
-// describe("USER-ENDPOINT-TEST", () => {
-//   it("should accept user to login", (done) => {
-//     request(app)
-//       .post("api/v1/users/login")
-//       .send({
-//         email: "yangeney@gmail.com",
-//         password: "yange@1!",
-//       })
-//       .end((err, res) => {
-//         expect(res.status).to.equal(200);
-//         done();
-//       });
-//   });
-//   it("should not accept user to login", (done) => {
-//     request(app)
-//       .post("api/v1/users/longin")
-//       .send({
-//         email: "yangeney@gmail.com",
-//         password: "yange@1!",
-//       })
-//       .end((err, res) => {
-//         expect(res.status).to.equal(200);
-//         done();
-//       });
-//   });
-// });
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+import app from "../src/app.js";
+import "dotenv/config";
+chai.use(chaiHttp);
+describe("USER-ENDPOINT-TEST", () => {
+  it("should accept user to login", (done) => {
+    request(app)
+      .post("api/v1/users/login")
+      .send({
+        email: "exampl@gmail.com",
+        password: "exampl12@!",
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+  it("should not accept user to login", (done) => {
+    request(app)
+      .post("api/v1/users/longin")
+      .send({
+        email: "yangeney@gmail.com",
+        password: "yange@1!",
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
